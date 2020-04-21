6270333e59ba996d6666c1a167b945f872361931
xuqinchao <15558048671@163.com>
2020-04-21 15:37:21 +0800

    体验

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-config.xml");
+        Book book = applicationContext.getBean("book", Book.class);
+        System.out.println(book.toString());
+    }
+}
